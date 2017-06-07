a7fc464c25d745a192c89ae3bd533c0dc5948fac
zackles007 <zachbragg@outlook.com>
2017-06-07 17:26:59 +1200

    Added code to spawn character, but it dosen't spawn :unamused:

--- zachGame.py
+++ zachGame.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 # by zackles007
 # thanks to Jess Weichler
-
 import pygame #load pygame keywords
 import sys #let python use your file system
 import os #help python identify your OS
 
 '''OBJECTS'''
-#put classes & functions here
+#put classes & functions here 
 
-
-
+class Player(pygame.sprite.Sprite):
+        #spawn a player
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.images = [ ]
+            img = pygame.image.load(os.path.join('images','hero.png')).convert()
+            self.images.append(img)
+            self.image = self.images[0]
+            self.rect = self.image.get_rect()
+            
+ 
 '''SETUP'''
 #code runs once
 screenX = 480 #screen width
@@ -27,8 +35,15 @@ screen = pygame.display.set_mode([screenX,screenY])
 backdrop = pygame.image.load(os.path.join('images','stage.png')).convert()
 backdropRect = screen.get_rect()
 
-'''MAIN LOOP'''
-#code runs many times
+player = Player() #spawn player on screen 
+player.rect.x = 0
+player.rect.y = 0
+movingsprites = pygame.sprite.Group()
+movingsprites.add(player)
+
+
+'''MAIN LOOP''' 
+#code runs many times 
 
 while main == True:
     for event in pygame.event.get():
@@ -38,7 +53,6 @@ while main == True:
                 sys.exit()
                 main = False
     screen.blit(backdrop, backdropRect)
-
     pygame.display.flip()
     clock.tick(fps)
     
