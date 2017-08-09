cd01b0ea741c2fdaaf672740e48fe0b1b6d46c1c
Cyanide Cupcake <jlweichler@gmail.com>
2017-08-09 20:09:48 +1200

    Fixed Indentation
    Fix an error that was preventing game from running.    Not sure what
    happened, but the indentation was out of whack across much of the code.

--- zachGame.py
+++ zachGame.py
@@ -12,129 +12,129 @@ import os #help python identify your OS
 class Player(pygame.sprite.Sprite):
         #spawn a player
         def __init__(self):
-            pygame.sprite.Sprite.__init__(self)
-            self.momentumX = 0 #move along X
-            self.momentumY = 0 #move along Y
+                pygame.sprite.Sprite.__init__(self)
+                self.momentumX = 0 #move along X
+                self.momentumY = 0 #move along Y
 
-            #gravity variables
-            self.collide_delta = 0
-            self.jump_delta = 6
+                #gravity variables
+                self.collide_delta = 0
+                self.jump_delta = 6
 
-            self.score = 0 #set score
-            self.images = [ ]
-            img = pygame.image.load(os.path.join('images','hero.png')).convert()
-            self.images.append(img)
-            self.image = self.images[0]
-            self.rect = self.image.get_rect()
-            self.image.convert_alpha() #optimise for alpha
-            self.image.set_colorkey(alpha) #set alpha
+                self.score = 0 #set score
+                self.images = [ ]
+                img = pygame.image.load(os.path.join('images','hero.png')).convert()
+                self.images.append(img)
+                self.image = self.images[0]
+                self.rect = self.image.get_rect()
+                self.image.convert_alpha() #optimise for alpha
+                self.image.set_colorkey(alpha) #set alpha
+
         def control(self, x, y):
-            #control player movement
-            self.momentumX += x
-            self.momentumY += y
+                #control player movement
+                self.momentumX += x
+                self.momentumY += y
 
         def update(self, enemy_list, platform_list):
-            #update sprite position
-            currentX = self.rect.x
-            nextX = currentX + self.momentumX
-            self.rect.x = nextX
+                #update sprite position
+                currentX = self.rect.x
+                nextX = currentX + self.momentumX
+                self.rect.x = nextX
 
-            currentY = self.rect.y
-            nextY = currentY + self.momentumY
-            self.rect.y = nextY
+                currentY = self.rect.y
+                nextY = currentY + self.momentumY
+                self.rect.y = nextY
 
-            #gravity
-            if self.collide_delta < 6 and self.jump_delta < 6:
-                self.jump_delta = 6*2
-                self.momentumY -=33 #how high to jump
-
-                self.collide_delta +=6
-                self.jump_delta += 6
+                #gravity
+                if self.collide_delta < 6 and self.jump_delta < 6:
+                        self.jump_delta = 6*2
+                        self.momentumY -=33 #how high to jump
+                        
+                        self.collide_delta +=6
+                        self.jump_delta += 6
 
         def jump (self, platform_list):
-             self.jump_delta = 0
+                self.jump_delta = 0
 
-            #colisions
-            enemy_hit_list = pygame.sprite.spritecollide(self, enemy_list, False)       
-            for enemy in enemy_hit_list:
-                self.score -= 1
-                print(self.score)
+                #colisions
+                enemy_hit_list = pygame.sprite.spritecollide(self, enemy_list, False)       
+                for enemy in enemy_hit_list:
+                        self.score -= 1
+                        print(self.score)
 
-            block_hit_list = pygame.sprite.spritecollide(self, platform_list, False)
-            if self.momentumX > 0:
-                 for block in block_hit_list:
-                     self.rect.y = currentY
-                     self.rect.x = currentX+9
-                     self.momentumY = 0
-                     self.collide_delta = 0 #stop jumping
+                block_hit_list = pygame.sprite.spritecollide(self, platform_list, False)
+                if self.momentumX > 0:
+                        for block in block_hit_list:
+                                self.rect.y = currentY
+                                self.rect.x = currentX+9
+                                self.momentumY = 0
+                                self.collide_delta = 0 #stop jumping
 
-            if self.momentumY > 0:
-                 for block in block_hit_list:
-                     self.rect.y = currentY
-                     self.momentumY = 0
-                     self.collide_delta = 0 #stop jumpig
+                if self.momentumY > 0:
+                        for block in block_hit_list:
+                                self.rect.y = currentY
+                                self.momentumY = 0
+                                self.collide_delta = 0 #stop jumpig
 
         def gravity(self):
-            self.momentumY += 2 #how fast player falls
+                self.momentumY += 2 #how fast player falls
 
-            if self.rect.y > 360 and self.momentumY >= 0:
-                self.momentumY = 0
-                self.rect.y = screenY-20
+                if self.rect.y > 360 and self.momentumY >= 0:
+                        self.momentumY = 0
+                        self.rect.y = screenY-20
 
 class Platform(pygame.sprite.Sprite):
-   #x location, y location, img width, img height, img file)
-   def __init__(self,xloc,yloc,imgw, imgh, img):
-       pygame.sprite.Sprite.__init__(self)
-       self.image = pygame.Surface([imgw, imgh])
-       self.image.convert_alpha()
-       self.image.set_colorkey(alpha)
-       self.blockpic = pygame.image.load(img).convert()
-       self.rect = self.image.get_rect()
-       self.rect.y = yloc
-       self.rect.x = xloc
-
-       #paint image into blocks
-       self.image.blit(self.blockpic,(0,0),(0,0,imgw,imgh))
-
-
-   def level1():
-       #create level 1
-       platform_list = pygame.sprite.Group()
-       block = Platform(260, 270, 58, 51,os.path.join('images','crate0.png'))
-       platform_list.add(block) #after each block
-       block = Platform(0, 320, 94, 49,os.path.join('images','ground_grass.png'))
-       platform_list.add(block) #after each block
-       block = Platform(94, 320, 94, 49,os.path.join('images','ground_grass.png'))
-       platform_list.add(block) #after each block
-       block = Platform(188, 320, 94, 49,os.path.join('images','ground_grass.png'))
-       platform_list.add(block) #after each block
-       block = Platform(282, 320, 70, 49,os.path.join('images','ground_grass.png'))
-       platform_list.add(block) #after each block
-
-       return platform_list #at end of function level1
-
+        #x location, y location, img width, img height, img file)
+        def __init__(self,xloc,yloc,imgw, imgh, img):
+                pygame.sprite.Sprite.__init__(self)
+                self.image = pygame.Surface([imgw, imgh])
+                self.image.convert_alpha()
+                self.image.set_colorkey(alpha)
+                self.blockpic = pygame.image.load(img).convert()
+                self.rect = self.image.get_rect()
+                self.rect.y = yloc
+                self.rect.x = xloc
+                
+                #paint image into blocks
+                self.image.blit(self.blockpic,(0,0),(0,0,imgw,imgh))
+                
+        def level1():
+                #create level 1
+                platform_list = pygame.sprite.Group()
+                block = Platform(260, 270, 58, 51,os.path.join('images','crate0.png'))
+                platform_list.add(block) #after each block
+                block = Platform(0, 320, 94, 49,os.path.join('images','ground_grass.png'))
+                platform_list.add(block) #after each block
+                block = Platform(94, 320, 94, 49,os.path.join('images','ground_grass.png'))
+                platform_list.add(block) #after each block
+                block = Platform(188, 320, 94, 49,os.path.join('images','ground_grass.png'))
+                platform_list.add(block) #after each block
+                block = Platform(282, 320, 70, 49,os.path.join('images','ground_grass.png'))
+                platform_list.add(block) #after each block
+                
+                return platform_list #at end of function level1
+        
 class Enemy(pygame.sprite.Sprite):
-    #spawn an enemy
-    def __init__(self,x,y,img): 
-       pygame.sprite.Sprite.__init__(self)
-       self.image = pygame.image.load(os.path.join('images', img))
-       self.image.convert_alpha()
-       self.image.set_colorkey(alpha)
-       self.rect = self.image.get_rect()
-       self.rect.x = x
-       self.rect.y = y
-       self.counter = 0 #counter variable
-    def move(self):
-        #enemy movement
-        if self.counter >= 0 and self.counter <= 30:
-            self.rect.x += 2
-        elif self.counter >= 30 and self.counter <=60:
-            self.rect.x -= 2
-        else:
-            self.counter = 0
-            print('reset')
-
-        self.counter += 1
+        #spawn an enemy
+        def __init__(self,x,y,img): 
+                pygame.sprite.Sprite.__init__(self)
+                self.image = pygame.image.load(os.path.join('images', img))
+                self.image.convert_alpha()
+                self.image.set_colorkey(alpha)
+                self.rect = self.image.get_rect()
+                self.rect.x = x
+                self.rect.y = y
+                self.counter = 0 #counter variable
+        def move(self):
+                #enemy movement
+                if self.counter >= 0 and self.counter <= 30:
+                        self.rect.x += 2
+                elif self.counter >= 30 and self.counter <=60:
+                        self.rect.x -= 2
+                else:
+                        self.counter = 0
+                        print('reset')
+                        
+                self.counter += 1
             
 '''SETUP'''
 #code runs once
